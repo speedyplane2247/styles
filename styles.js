564e08cf447c809dbb0d88a9cd53c7941edce17d
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-24 21:40:59 -0500

    Update styles.js

--- styles.js
+++ styles.js
@@ -3,3 +3,6 @@ styles.js for JavaScript
 (c) speedyplane2247 2018-2019
 GNU License 2.
 */
+var winStyler = new Object();
+winStyler.version = "v1.0-2880-master";
+winStyler.marketminVer = "v1.0-2879-market";
